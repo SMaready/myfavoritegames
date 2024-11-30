09c27407870e810b2ac63059eeb2f24b4ec0b4a8
Stephan Maready <stephan.maready@gmail.com>
2024-11-29 18:08:21 -0800

    Added detailUrl to DB Schema

--- api/db_schema.sql
+++ api/db_schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE games(
     name VARCHAR(100) NOT NULL,
     genreId INT NOT NULL,
     imageUrl VARCHAR(200) NOT NULL,
+    detailUrl VARCHAR(200) NOT NULL,
     PRIMARY KEY (Id)
 );
 
@@ -28,25 +29,25 @@ INSERT INTO genres(id, name, imageUrl, description) VALUES (4, 'Puzzle', '/photo
 INSERT INTO genres(id, name, imageUrl, description) VALUES (5, 'Survival', '/photos/survival_background_image.jpeg', '');
 INSERT INTO genres(id, name, imageUrl, description) VALUES (6, 'MMO', '/photos/mmo_background_image.webp', '');
 
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (1, 'Black Ops', 1, '/photos/black_ops_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (2, 'Halo 3', 1, '/photos/halo3_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (3, 'Halo 3: ODST', 1, '/photos/halo_odst_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (4, 'Modern Warfare', 1, '/photos/cod4_background_image.jpg');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (1, 'Black Ops', 1, '/photos/black_ops_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (2, 'Halo 3', 1, '/photos/halo3_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (3, 'Halo 3: ODST', 1, '/photos/halo_odst_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (4, 'Modern Warfare', 1, '/photos/cod4_background_image.jpg', '');
 
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (5, 'Batman: Arkham', 2, '/photos/arkham_knight_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (6, 'Uncharted 3', 2, '/photos/uncharted3_background_image.jpg');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (5, 'Batman: Arkham', 2, '/photos/arkham_knight_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (6, 'Uncharted 3', 2, '/photos/uncharted3_background_image.jpg', '');
 
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (7, 'FIFA 15', 3, '/photos/fifa15_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (8, 'NBA 2K16', 3, '/photos/nba2k16_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (9, 'Super Mario Strikers', 3, '/photos/supermariostrikers_background_image.png');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (7, 'FIFA 15', 3, '/photos/fifa15_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (8, 'NBA 2K16', 3, '/photos/nba2k16_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (9, 'Super Mario Strikers', 3, '/photos/supermariostrikers_background_image.png', '');
 
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (10, 'Portal 2', 4, '/photos/portal2_background_image.jpeg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (11, 'Call of the Sea', 4, '/photos/call_of_the_sea_background_image.png');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (12, 'LittleBigPlanet3', 4, '/photos/littlebigplanet_3_background_image.avif');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (10, 'Portal 2', 4, '/photos/portal2_background_image.jpeg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (11, 'Call of the Sea', 4, '/photos/call_of_the_sea_background_image.png', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (12, 'LittleBigPlanet3', 4, '/photos/littlebigplanet_3_background_image.avif', '');
 
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (13, 'Minecraft', 5, '/photos/minecraft_background_image.webp');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (14, 'DayZ', 5, '/photos/dayz_background_image.jpg');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (13, 'Minecraft', 5, '/photos/minecraft_background_image.webp', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (14, 'DayZ', 5, '/photos/dayz_background_image.jpg', '');
 
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (15, 'World of Warcraft', 6, '/photos/wow_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (16, 'Dungeons and Dragons Online', 6, '/photos/ddo_background_image.jpg');
-INSERT INTO games(id, name, genreId, imageUrl) VALUES (17, 'Elder Scrolls Online', 6, '/photos/eso_background_image.avif');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (15, 'World of Warcraft', 6, '/photos/wow_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (16, 'Dungeons and Dragons Online', 6, '/photos/ddo_background_image.jpg', '');
+INSERT INTO games(id, name, genreId, imageUrl, detailUrl) VALUES (17, 'Elder Scrolls Online', 6, '/photos/eso_background_image.avif', '');
